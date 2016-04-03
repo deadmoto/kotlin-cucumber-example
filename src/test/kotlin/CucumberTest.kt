c1cc615422d4647ceadac9a586ca38ca916e16e4
Dmitry Berezhnoy <deadmoto@gmail.com>
2016-04-02 23:58:34 -0700

    Fixed problem with concurrent plugin initialization.

--- src/test/kotlin/CucumberTest.kt
+++ src/test/kotlin/CucumberTest.kt
@@ -1,5 +1,8 @@
 import cucumber.api.CucumberOptions
 import cucumber.api.testng.AbstractTestNGCucumberTests
+import cucumber.api.testng.TestNGCucumberRunner
+import cucumber.runtime.RuntimeOptions
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.DataProvider
 import org.testng.annotations.Test
 
@@ -11,8 +14,34 @@ import org.testng.annotations.Test
 )
 @Test class CucumberTest() : AbstractTestNGCucumberTests() {
 
+    @BeforeClass(alwaysRun = true)
+    override fun setUpClass() {
+        super.setUpClass()
+        testNGCucumberRunner.runtimeOptions.getPlugins()
+    }
+
     @DataProvider(parallel = true)
     override fun features(): Array<Array<Any>> {
         return super.features()
     }
+}
+
+val AbstractTestNGCucumberTests.testNGCucumberRunner: TestNGCucumberRunner
+    get() {
+        val declaredField = AbstractTestNGCucumberTests::class.java.getDeclaredField("testNGCucumberRunner")
+        declaredField.isAccessible = true
+        return declaredField.get(this) as TestNGCucumberRunner
+    }
+
+val TestNGCucumberRunner.runtimeOptions: RuntimeOptions
+    get() {
+        val declaredField = TestNGCucumberRunner::class.java.getDeclaredField("runtimeOptions")
+        declaredField.isAccessible = true
+        return declaredField.get(this) as RuntimeOptions
+    }
+
+fun RuntimeOptions.getPlugins() {
+    val declaredMethod = RuntimeOptions::class.java.getDeclaredMethod("getPlugins")
+    declaredMethod.isAccessible = true
+    declaredMethod.invoke(this)
 }
